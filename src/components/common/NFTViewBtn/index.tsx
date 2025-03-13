547c2e08e6223450e4ce00cea4d2e21be2735d74
kaiserkernel <kaiserkernel84@gmail.com>
2025-03-13 14:19:09 +0100

    feat: buy btn

--- src/components/common/NFTViewBtn/index.tsx
+++ src/components/common/NFTViewBtn/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react"
+import { motion } from "framer-motion"
 
 import { NFTMetaData, NFTProps } from "../../../types"
 import { fetchMetaData } from "../../../services/metaDataService";
@@ -6,6 +7,13 @@ import { fetchMetaData } from "../../../services/metaDataService";
 export const NFTViewBtn = ({data}: {data: NFTProps}) => {
     const { price, tokenURI  } = data;
     const [nftData, setNFTData] = useState<NFTMetaData | null>(null);
+
+    const handleBuyNft = () => {
+        if (!price) return;
+        console.log("mft")
+        console.log(data, "data")
+        console.log(nftData, "nftdata")
+    }
 
     useEffect(() => {
         const fetchNFTData = async () => {
@@ -22,23 +30,42 @@ export const NFTViewBtn = ({data}: {data: NFTProps}) => {
     return (
         <button
             // onClick={() => handleBtnClick(collection)}
-            className="relative md:w-64 md:h-64 group w-[40vw] h-[40vw]"
+            className="relative group"
         >
           {
               nftData?.image && (
-                  <div className="relative w-full h-full group rounded-md border-4 border-white border-opacity-30 transition-all duration-500 hover:border-opacity-100 hover:shadow-lg">
+                <div className="relative w-full h-full group rounded-2xl border-4 border-white border-opacity-30 transition-all duration-500 hover:border-opacity-100 hover:shadow-lg">
                     {/* Image with Hover Blur Effect */}
                     <img 
                         src={nftData.image} 
                         alt={nftData.name} 
-                        className="w-full h-full object-cover transition duration-300 group-hover:blur-sm"
+                        className="w-full h-full object-cover transition duration-300 group-hover:blur-sm w-[40vw] h-[40vw] md:w-64 md:h-64 rounded-t-xl"
                     />
 
-                    {/* Text Fades in on Hover if not confirmed*/}
-                    <span className="absolute inset-0 flex items-center justify-center text-white text-lg font-bold opacity-0 transition duration-300 group-hover:opacity-100">
-                        Buy
-                    </span>
-                  </div>
+                    {/* NFT Info */}
+                    <div className="w-[40vw] md:w-64 flex flex-col text-white items-start">
+                        <p className="md:text-sm text-xs font-light mt-3 ml-4">{nftData.name}</p>
+                        <p className={`md:text-base text-sm font-medium mt-2 ml-4 pb-10 ${price === 0 ? 'text-blue-600' : ''}`}>
+                            {price === 0 ? "Not for sale" : `${price}ETH`}
+                        </p>
+                    </div>
+
+                    {/* Button appears when hovering the entire component */}
+                    <div 
+                        className={`absolute bottom-0 left-0 w-full h-[2.5rem] flex justify-center items-center bg-blue-600 text-white text-sm font-medium rounded-b-md opacity-0 ${price !== 0 ? 'group-hover:opacity-100 group-hover:translate-y-0 group-hover:h-[2.5rem] transition-all duration-300' : ''} rounded-b-xl grid grid-cols-9`} 
+                        onClick={handleBuyNft}
+                    >
+                        <span className="col-span-6 mx-auto font-bold">Buy Now</span>
+                        
+                        {/* <!-- Vertical white divider --> */}
+                        <div className="h-6 border-r-2 border-white mx-2"></div>
+                        
+                        {/* <!-- Image aligned to the right --> */}
+                        <img className="col-span-2 h-6 w-6 mx-auto" src="/buy.webp" />
+                    </div>
+
+
+                </div>
               )
           }
       </button>
