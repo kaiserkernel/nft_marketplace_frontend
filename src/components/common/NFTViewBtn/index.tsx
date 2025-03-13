e5a691b96f21d911ec11bfc088a52a574d71790b
kaiserkernel <kaiserkernel84@gmail.com>
2025-03-13 15:22:28 +0100

    style: view nft btn

--- src/components/common/NFTViewBtn/index.tsx
+++ src/components/common/NFTViewBtn/index.tsx
@@ -32,7 +32,6 @@ export const NFTViewBtn = ({nftData, setNFTList}: NFTViewBtnProps) => {
         }
 
         setIsProcessing(true);
-
         try {
             // Estimate the gas required for the transaction
             const gasEstimate = await collectionContract.mintNFT.estimateGas( nftData.tokenId );
